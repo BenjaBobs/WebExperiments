96b0aa48c23e39ce90120cf495dc6c7669154175
Benjamin <behub92@gmail.com>
2021-01-03 17:26:01 +0100

    Test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Does it work?</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        or does it not?
       </header>
     </div>
   );
